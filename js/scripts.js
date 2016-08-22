e6b7a9552961d4f0a8a0e8f8bea12a9b3bd96a84
Kyle Fisher and Samuel Peppard <kyle@>
2016-08-22 11:07:31 -0400

    add address fields and address functionality for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 //business
-function Contact(first, last) {
+function Contact(first, last, city, state) {
   this.nameFirst = first;
   this.nameLast = last;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.nameFirst + " " + this.nameLast;
+}
+
+Contact.prototype.address = function() {
+  return this.city + ", " + this.state;
 }
 
 //user
@@ -11,15 +21,25 @@ $(document).ready(function() {
 
     var userNameFirst = $("input#new-name-first").val();
     var userNameLast = $("input#new-name-last").val();
+    var userAddressCity = $("input#new-address-city").val();
+    var userAddressState = $("input#new-address-state").val();
 
-    var newContact = new Contact(userNameFirst, userNameLast);
+    var newContact = new Contact(userNameFirst, userNameLast, userAddressCity, userAddressState);
 
-    $("ul#contacts").append("<li><span class = 'contact'>" + newContact.nameFirst + "</span></li>");
+    $("ul#contacts").append("<li><span class = 'contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.nameFirst);
+      $(".first-name").text(newContact.nameFirst);
+      $(".last-name").text(newContact.nameLast);
+      $(".address").text(newContact.address());
+    });
 
     $("input#new-name-first").val("");
     $("input#new-name-last").val("");
-
-    console.log(newContact.nameFirst);
+    $("input#new-address-city").val("");
+    $("input#new-address-state").val("");
 
   });
 });
